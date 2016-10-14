c07e111bfc05d51ca67d966270b4f4e4952cf836
Samuel Phillips <samphillips1879@gmail.com>
2016-10-14 15:54:30 -0500

    end of day 1
    basic html structure intact, js barely started

---
+++ converter.js
@@ -0,0 +1,36 @@
+
+
+
+
+var button = document.getElementById("convertButton");
+
+
+
+
+
+button.addEventListener("click", determineConverter);
+
+
+
+
+
+
+function determineConverter(clickEvent) {
+	console.log("event", clickEvent);
+}
+
+
+
+
+
+function toCelsius () {
+
+}
+
+
+
+
+
+function toFahrenheit () {
+
+}
